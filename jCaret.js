a9593ef8280947ad7bff03f564d396140b846630
Hamzah JA <hamzajaayash@gmail.com>
2025-11-26 18:48:59 +0300

    Update jCaret.js

--- jCaret.js
+++ jCaret.js
@@ -672,6 +672,7 @@ class jCaret {
                     <svg class="w-6 h-6 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     <h3 class="text-lg font-medium ">${this.i18n.infoTitle}</h3>
                 </div>
+                <br/>
                 <div id="aboutText" class="flex items-center gap-3 mb-5 border-b pb-3 border-gray-300">
                     <h3 class="text-lg font-medium text-gray-800">${this.i18n.infoText}</h3>
                 </div>
